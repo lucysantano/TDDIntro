67743e13ff8489a49dfd078584d980667a8a66a2
Lucy Santano <lucysantano@sympatico.ca>
2015-09-08 15:40:05 -0400

    Finished Factorial

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,17 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if(i<0){
+            throw new IllegalArgumentException();
+        }
+        else if(i==0)
+            return 1;
+        else {
+            int total = 1;
+            for(int j=1; j<=i; j++){
+                total*=j;
+            }
+            return total;
+        }
     }
 }
