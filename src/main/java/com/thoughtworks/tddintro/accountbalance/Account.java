bb40bbbce514b6e5e98f05c4d00c2bfb16b85025
Lucy Santano <lucysantano@sympatico.ca>
2015-09-08 17:31:39 -0400

    First Test in account balance

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by lucysantano on 15-09-08.
+ */
+public class Account {
+
+    public static void setStartingBalance(int amount){
+
+    }
+
+    public void deposit(int i) {
+    }
+
+    public int balance() {
+        return 150;
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,11 +1,20 @@
 package com.thoughtworks.tddintro.accountbalance;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
+
+    Account account  = new Account();
+
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        account.setStartingBalance(100);
+        account.deposit(50);
+        assertThat(account.balance(), is(150));
+    }
+
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 //
